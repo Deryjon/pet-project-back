1b2be1d7db387c5ee9102f7f34b8c1ba9cee7045
Deryjon <yusupoviskandar1005@gmail.com>
2026-04-05 09:14:11 +0000

    feat: add c hanges

--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-import './src/main';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './src/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+  origin: true,
+  credentials: true,
+})
 
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Konkurent CRM API')
-    .setDescription('API documentation for Konkurent CRM')
+    .setDescription('API documentation')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
