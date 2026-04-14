574f95f284d725d6432b1d5acb1281e01a7039ec
Dery Jon <133222583+Deryjon@users.noreply.github.com>
2026-04-14 16:49:58 +0500

    fix(shop): scope /shop by auth context and only_allowed

--- src/company-settings/company-settings.module.ts
+++ src/company-settings/company-settings.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CompanySettingsController } from './company-settings.controller';
 import { CompanySettingsService } from './company-settings.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [CompanySettingsController],
   providers: [CompanySettingsService],
   exports: [CompanySettingsService],
