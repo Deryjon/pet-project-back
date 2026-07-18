d3053aa30d7ff445dadb13623f6bb5e2d69f0086
Deryjon <yusupoviskandar1005@gmail.com>
2026-07-18 14:36:20 +0000

    feat: add changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,9 @@ module.exports = {
     {
       name: "konkurent-api",
       script: "dist/main.js",
+      cwd: "/home/deryjon/pet-project-back",
       env: {
-        NODE_ENV: "production",
-        JWT_SECRET: "supersecretkey123",
-        DATABASE_URL: "postgresql://crm_user:crm_password@localhost:5432/crm_db?schema=public"
+        NODE_ENV: "production"
       }
     }
   ]
